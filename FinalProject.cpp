4f5e8bb31477d38373e0be6a2fcc2ab14d11305b
AbdullahRavian <abdullahghulamnabi18@gmail.com>
2022-06-01 23:08:35 +0500

    Update FinalProject.cpp

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -39,20 +39,47 @@ cin.ignore();
 system ("cls");
 if(choice == 'A' || choice=='a'){
     enter_data();
-
+	cout<<"Do you want to end?(if yes press Y otherwise press N): ";
+	cin>>choice;
+	if(choice=='y'||choice=='Y'){
+		break;
+	}
 }
     
  if(choice == 'B' || choice== 'b'){
 	search();
+	cout<<"Do you want to end?(if yes press Y otherwise press N): ";
+	cin>>choice;
+	if(choice=='y'||choice=='Y'){
+		break;
+	}	
 } 
 
 
  if(choice == 'C'|| choice== 'c'){
-	update();
+ 	
+	delete_data();
+	remove("bds_donor_data.txt");
+
+    rename("temp.txt","bds_donor_data.txt");
+    cout<<"Data deleted succesfully!"<<endl<<endl;
+		cout<<"Do you want to end?(if yes press Y otherwise press N): ";
+	cin>>choice;
+	if(choice=='y'||choice=='Y'){
+		break;
+	}
+
 }
+	
+
 
  if(choice == 'D'|| choice== 'd'){
-	delete_data();
+	update();
+		cout<<"Do you want to end?(if yes press Y otherwise press N): ";
+	cin>>choice;
+	if(choice=='y'||choice=='Y'){
+		break;
+	}
 }
 
 }
@@ -125,7 +152,95 @@ void search(){
     read.close();
 }
 
+void delete_data(){
+	donor d[100];
+		ifstream read;
+		read.open("bds_donor_data.txt",ios::in);
+		ofstream write;
+		write.open("temp.txt",ios::app | ios::out);
+		
+	cout<<"Enter the name of Student you want to delete the record of:";
+	string name;
+	getline(cin,name);
+	while(read){
+	    for(int i=0;i<100;i++){
+	    	 getline(read,d[i].id);
+			 getline(read,d[i].name);
+			 getline(read,d[i].address);
+			 getline(read,d[i].phone);
+			 getline(read,d[i].blood_group);
+			 getline(read,d[i].date);
+		    if(name!=d[i].name){
+		    	
+		  write<<d[i].id;
+		  write<<endl;
+		  write<<d[i].name;
+		  write<<endl;
+		  write<<d[i].address;
+		  write<<endl;
+		  write<<d[i].phone;
+		  write<<endl;
+		  write<<d[i].blood_group;
+		  write<<endl;
+		  write<<d[i].date;
+		}
+	}
+  write.close();
+  }
 
+}
+
+
+
+
+void update(){
+
+	donor d[100];
+	cout<<"Enter the ID of donor whose Data you want to update :";
+	ifstream read;
+	read.open("bds_donor_data.txt" ,ios::out);
+	ofstream write;
+	write.open("bds_donor_data.txt");
+	string id;
+	cin>>id;
+  while(read){
+	     for(int i=0;i<100;i++){
+             getline(read,d[i].id);
+	         getline(read,d[i].name);
+             getline(read,d[i].address);
+	         getline(read,d[i].phone);
+             getline(read,d[i].blood_group);
+	         getline(read,d[i].date);
+			
+			     if(id==d[i].id){
+				  cout<<"Not found such id"<<endl;
+			     	break;
+			     }
+			      else{
+			     	cout<<"Enter the updated data for donor: "<<i+1<<endl<<endl;
+			      	cout<<"Enter New ID :";
+			     	cin>>d[i].id;
+			     	cout<<"Enter New Name :";
+			     	cin>>d[i].id;
+			     	cout<<"Enter New Address :";
+			     	cin>>d[i].id;
+			     	cout<<"Enter New Phone NO.:";
+			     	cin>>d[i].id;
+			     	cout<<"Enter New BLood Group :";
+			     	cin>>d[i].id;
+			     	cout<<"Enter New Date :";
+			     	cin>>d[i].id;
+                     write<<d[i].id<<endl<<d[i].name<<endl<<d[i].address<<endl<<d[i].phone<<endl<<d[i].blood_group<<endl<<d[i].date<<endl;
+
+			     	break;
+			     	
+				
+			}
+		}
+	
+    } 
+	write.close();	
+}
 
 
 
